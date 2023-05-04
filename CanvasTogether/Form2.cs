dd56ced7b8810ba63ceb0ddc5de6d65f173f8af2
minbasubo <suwh2600@naver.com>
2023-05-04 13:25:36 +0900

    nothing

--- CanvasTogether/Form2.cs
+++ CanvasTogether/Form2.cs
@@ -21,5 +21,22 @@ namespace CanvasTogether
         {
             this.Close();
         }
+
+        public void Message(string message)
+        {
+            this.Invoke(new MethodInvoker(delegate () //크로스 스레드 문제 예방
+            {
+                chatUI.AppendText(message + "\r\n"); //채팅 목록창에 문자열 추가
+                chatUI.Focus();
+                chatUI.ScrollToCaret(); //현재 캐럿의 위치로 윈도우 스크롤 이동
+                txtSend.Focus();
+            }));
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
