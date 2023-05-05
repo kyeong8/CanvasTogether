ef34c9048e09990edb78019a8f61cd0046496acf
minbasubo <suwh2600@naver.com>
2023-05-05 20:43:11 +0900

    make Sign in, Sign up function using SQLite

--- Client/ClientForm.Designer.cs
+++ Client/ClientForm.Designer.cs
@@ -28,77 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnDisconnect = new System.Windows.Forms.Button();
-<<<<<<< HEAD:CanvasTogether/Form2.Designer.cs
-            this.chatUI = new System.Windows.Forms.TextBox();
-            this.btnSend = new System.Windows.Forms.Button();
-            this.txtSend = new System.Windows.Forms.TextBox();
-=======
             this.txtChat = new System.Windows.Forms.TextBox();
             this.txtInput = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
->>>>>>> 2af93a44c36ea2203ad57ad2c3ac182f4fc4efb0:Client/ClientForm.Designer.cs
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(680, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "오른쪽 채팅 영역";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(283, 330);
+            this.label2.Location = new System.Drawing.Point(248, 264);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 15);
+            this.label2.Size = new System.Drawing.Size(97, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "왼쪽 그림판 영역";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(638, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "사이즈는 300, 600으로 해주세요";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(242, 359);
+            this.label4.Location = new System.Drawing.Point(212, 287);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(225, 15);
+            this.label4.Size = new System.Drawing.Size(177, 12);
             this.label4.TabIndex = 3;
             this.label4.Text = "사이즈는 600, 600으로 해주세요";
             // 
             // btnDisconnect
             // 
-            this.btnDisconnect.Location = new System.Drawing.Point(467, 658);
-            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDisconnect.Location = new System.Drawing.Point(409, 526);
             this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(86, 29);
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
             this.btnDisconnect.TabIndex = 4;
             this.btnDisconnect.Text = "연결 종료";
             this.btnDisconnect.UseVisualStyleBackColor = true;
             this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
             // 
-<<<<<<< HEAD:CanvasTogether/Form2.Designer.cs
-            // chatUI
-            // 
-            this.chatUI.Location = new System.Drawing.Point(708, 12);
-            this.chatUI.Multiline = true;
-            this.chatUI.Name = "chatUI";
-            this.chatUI.Size = new System.Drawing.Size(300, 600);
-            this.chatUI.TabIndex = 5;
-            // 
-            // btnSend
-            // 
-            this.btnSend.Location = new System.Drawing.Point(814, 666);
-            this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.TabIndex = 6;
-            this.btnSend.Text = "송신";
-            this.btnSend.UseVisualStyleBackColor = true;
-            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
-            // 
-            // txtSend
-            // 
-            this.txtSend.Location = new System.Drawing.Point(708, 627);
-            this.txtSend.Name = "txtSend";
-            this.txtSend.Size = new System.Drawing.Size(300, 25);
-            this.txtSend.TabIndex = 7;
-            // 
-            // Form2
-=======
             // txtChat
             // 
             this.txtChat.Location = new System.Drawing.Point(572, 12);
@@ -128,31 +113,19 @@
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // ClientForm
->>>>>>> 2af93a44c36ea2203ad57ad2c3ac182f4fc4efb0:Client/ClientForm.Designer.cs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD:CanvasTogether/Form2.Designer.cs
-            this.ClientSize = new System.Drawing.Size(1010, 701);
-            this.Controls.Add(this.txtSend);
-            this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.chatUI);
-=======
             this.ClientSize = new System.Drawing.Size(884, 561);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.txtChat);
->>>>>>> 2af93a44c36ea2203ad57ad2c3ac182f4fc4efb0:Client/ClientForm.Designer.cs
             this.Controls.Add(this.btnDisconnect);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-<<<<<<< HEAD:CanvasTogether/Form2.Designer.cs
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "Form2";
-=======
             this.Controls.Add(this.label1);
             this.Name = "ClientForm";
->>>>>>> 2af93a44c36ea2203ad57ad2c3ac182f4fc4efb0:Client/ClientForm.Designer.cs
             this.Text = "CanvasTogether";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClientForm_FormClosing);
             this.ResumeLayout(false);
@@ -161,17 +134,14 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnDisconnect;
-<<<<<<< HEAD:CanvasTogether/Form2.Designer.cs
-        private System.Windows.Forms.TextBox chatUI;
-        private System.Windows.Forms.Button btnSend;
-        private System.Windows.Forms.TextBox txtSend;
-=======
         private System.Windows.Forms.TextBox txtChat;
         private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.Button btnSend;
->>>>>>> 2af93a44c36ea2203ad57ad2c3ac182f4fc4efb0:Client/ClientForm.Designer.cs
     }
 }
---
+++ Client/SignUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class SignUp : Form
+    {
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignUP_Click(object sender, EventArgs e)
+        {
+            string route = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+            try
+            {
+                conn = new SQLiteConnection(@"Data Source=" + route + "\\Database\\cvtDB.db");
+                conn.Open();
+
+                cmd = new SQLiteCommand(conn);
+                cmd.CommandText = $"insert into member (id, pw, name) values (" +
+                    $"'{ID_txtbox.Text.Trim()}', '{PW_txtbox.Text.Trim()}', '{Name_txtbox.Text.Trim()}')";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("회원 가입 완료");
+
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("오류가 발생했습니다.");
+            }
+        }
+    }
+}
